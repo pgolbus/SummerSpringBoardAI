478a4a28c89c02d284c198a0a563969ec093342f
Peter B. Golbus <pgolbus@gmail.com>
2023-07-07 21:03:54 +0000

    legibility stuff

--- GA/build_knapsack.py
+++
@@ -1,6 +0,0 @@
-import random
-with open("knapsack.txt", "w") as fh:
-    fh.write("KNAPSACK = [")
-    for _ in range(10):
-        fh.write(f"({random.randint(1,9)}, {random.randint(9,99)}),")
-    fh.write("]")
